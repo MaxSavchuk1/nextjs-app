7c0322b0387a959ce693fd8f3ac7c51290d9fdcb
Maksym Savchuk <m.savchuk@urancompany.com>
2024-11-03 18:55:14 +0200

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
+// import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    // additionalData: `$var: red;`,
+    // includePaths: [path.join(__dirname, "styles")],
+    // prependData: `@import "variables.scss";`,
+    prependData: `@use 'src/styles/variables' as *;`,
+  },
 };
 
 export default nextConfig;
